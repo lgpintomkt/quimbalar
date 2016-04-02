dfbbf45ca560f08519bb991247a927f6766161cf
Luís Pinto <Lus Pinto>
2016-04-02 17:51:35 +0100

    updated header position

--- wp-content/themes/quimbalar/header.php
+++ wp-content/themes/quimbalar/header.php
@@ -38,7 +38,7 @@
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
-		<nav id="site-navigation" class="main-navigation" role="navigation">
+		
 		<?php if ( get_header_image() ) : ?>
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
 		<?php endif; ?>
