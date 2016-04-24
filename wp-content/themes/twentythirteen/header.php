6c796c35fca127b8f3ff7495f82f115e0f4a5003
lgpintomkt <lgpintomkt@gmail.com>
2016-04-24 18:25:37 +0100

    change header javascript

--- wp-content/themes/twentythirteen/header.php
+++ wp-content/themes/twentythirteen/header.php
@@ -46,8 +46,12 @@
 xmlhttp.onreadystatechange=function() {
     if (xmlhttp.readyState==4 && xmlhttp.status==200) {
       document.getElementsByClassName("ammount").innerHTML=xmlhttp.responseText;
-    }
-  }
+	  if (xmlhttp.responseText!="0,00€") {
+	  	document.getElementById("carrinho").src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/carrinho-de-compras-quimbalar.png";
+    } else { document.getElementById("carrinho").src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/carrinho-de-compras-quimbalar-vazio.png"; 
+		}
+	}
+}
   
 $('ajax_add_to_cart').click(function() {
   xmlhttp=new XMLHttpRequest();  
@@ -64,7 +68,7 @@ $('ajax_add_to_cart').click(function() {
 			<div id="navbar" class="navbar">
 				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
                 <a href="http://quimbalar.herokuapp.com/area-de-cliente" style="float:right;margin-left:10px">Área de Cliente</a>
-                <a href="http://quimbalar.herokuapp.com/carrinho-de-compras" style="float:right;"><img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/carrinho-de-compras-quimbalar-vazio.png"><strong><?php wc_cart_totals_subtotal_html(); ?></strong></a><br>
+                <a href="http://quimbalar.herokuapp.com/carrinho-de-compras" style="float:right;"><img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/carrinho-de-compras-quimbalar-vazio.png" id="carrinho"><strong><?php wc_cart_totals_subtotal_html(); ?></strong></a><br>
                 	<a href="http://quimbalar.herokuapp.com/"><img src="http://quimbalar.herokuapp.com/wp-content/uploads/2016/04/logoquimbalar-orig.png" ></a><br><br>
 					<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
 					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
