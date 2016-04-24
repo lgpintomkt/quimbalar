161b45cf8874b648de1e8724e81775637036f3c7
lgpintomkt <lgpintomkt@gmail.com>
2016-04-24 19:06:44 +0100

    fix xml generator php

--- cart-total-xml.php
+++ cart-total-xml.php
@@ -4,12 +4,15 @@ header('Content-type: text/xml');
 header('Pragma: public');
 header('Cache-control: private');
 header('Expires: -1');
+
+global $woocommerce; 
+
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
 
 echo '<xml>';
 
 echo '<total>';
-echo 12;
+echo $subtotal=$woocommerce->cart->get_cart_subtotal();
 echo '</total>';
 
 echo '</xml>';
