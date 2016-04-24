be2638e115d5476e7253aff983589a611a9f1b66
lgpintomkt <lgpintomkt@gmail.com>
2016-04-24 15:09:42 +0100

    change footer

--- wp-content/themes/twentythirteen/footer.php
+++ wp-content/themes/twentythirteen/footer.php
@@ -16,9 +16,11 @@
 
 			<div class="site-info">
 				Este  website  é  um projeto académico dos estudantes Luís Pinto, Ana Borges, Eunice Caeiro e Micael Duarte, da Licenciatura em Marketing da <a href="http://www.esce.ips.pt">ESCE</a>, não constituindo fonte de informação real da Quimbalar.
-			</div><!-- .site-info -->
+			
             
             <div style="float:right;"><img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/square-facebook-128.png"><img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/Youtube_Icon.png"></div>
+            
+            </div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
 
