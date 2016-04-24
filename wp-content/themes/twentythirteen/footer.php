585796955a4cb5e7111d0e4f2f9ded93acbf88fc
lgpintomkt <lgpintomkt@gmail.com>
2016-04-24 15:40:17 +0100

    change links footer

--- wp-content/themes/twentythirteen/footer.php
+++ wp-content/themes/twentythirteen/footer.php
@@ -18,9 +18,9 @@
 				Este  website  é  um projeto académico dos estudantes Luís Pinto, Ana Borges, Eunice Caeiro e Micael Duarte, da Licenciatura em Marketing da <a href="http://www.esce.ips.pt">ESCE</a>, não constituindo fonte de informação real da Quimbalar.
 			
             
-            <div style="float:left;"><img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/facebook-icon.png">&nbsp;&nbsp;&nbsp;<img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/youtube-icon.png"></div>
+            <div style="float:left;"><a href="https://www.facebook.com/Quimbalar/"><img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/facebook-icon.png"></a>&nbsp;&nbsp;&nbsp;<a href="https://www.youtube.com/channel/UCIf9AzfAzeW4XsYPYvf8bKQ"><img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/youtube-icon.png"></a></div>
             
-            <div style="float:right;"><img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/sgs-iso22000-icon.png">&nbsp;&nbsp;<img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/truste-verified-icon.png">&nbsp;&nbsp;<img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/norton-secured-powered-by-symantec-icon.png"></div>
+            <div style="float:right;"><a href="http://www.pt.sgs.com/pt/home_pt_v2?"><img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/sgs-iso22000-icon.png"></a>&nbsp;&nbsp;<a href="https://www.truste.com/business-products/trusted-websites/"><img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/truste-verified-icon.png"></a>&nbsp;&nbsp;<a href="https://www.symantec.com/ssl-certificates/"><img src="http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/norton-secured-powered-by-symantec-icon.png"></a></div>
             
             <br><br>
             
