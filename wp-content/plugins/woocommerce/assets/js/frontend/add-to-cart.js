9b65423ba099d8ac4295b36756295d3e023b9f2f
lgpintomkt <lgpintomkt@gmail.com>
2016-04-24 21:30:47 +0100

    test

--- wp-content/plugins/woocommerce/assets/js/frontend/add-to-cart.js
+++ wp-content/plugins/woocommerce/assets/js/frontend/add-to-cart.js
@@ -109,8 +109,8 @@ jQuery( function( $ ) {
 
 					// Trigger event so themes can refresh other areas
 					$( document.body ).trigger( 'added_to_cart', [ fragments, cart_hash, $thisbutton ] );
-					document.getElementsByClasses("ajaxtotal").innerHTML = "21,30€";
-					document.getElementsByClasses("carrinho").src = "http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/carrinho-de-compras-quimbalar.png";
+					document.getElementsByClassName("ajaxtotal").innerHTML = "21,30€";
+					document.getElementsByClassName("carrinho").src = "http://quimbalar.s3-eu-west-1.amazonaws.com/wp-content/uploads/2016/04/carrinho-de-compras-quimbalar.png";
 				}
 			});
 
